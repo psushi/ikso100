84c83fdcdd54d5bcc2c062dc8dd0488214185ba7
psushi <susheendharvj@gmail.com>
2025-06-13 19:17:12 -0500

    feat: correct COM orientation

--- aruco_detection.py
+++ aruco_detection.py
@@ -1,11 +1,14 @@
 import json
 import sys
+from typing import Sequence
 
 import cv2
 import numpy as np
 import numpy.typing as npt
 from cv2.aruco import estimatePoseSingleMarkers
 from cv2.typing import MatLike
+from numpy.matlib import mean
+from scipy.spatial.transform import Rotation as R
 
 NAME_TO_ID = {
     "top": 0,
@@ -34,7 +37,52 @@ trans_offset = {
     5: np.array([0.0, 0.0, -half_cube_width]),
 }
 
+rot_offset = {
+    0: np.eye(3),
+    1: np.eye(3),
+    2: R.from_euler("x", -90, degrees=True).as_matrix()
+    @ R.from_euler("z", 180, degrees=True).as_matrix(),
+    3: R.from_euler("x", -90, degrees=True).as_matrix(),
+    4: R.from_euler("x", -90, degrees=True).as_matrix()
+    @ R.from_euler("z", 90, degrees=True).as_matrix(),
+    5: R.from_euler("x", -90, degrees=True).as_matrix()
+    @ R.from_euler("z", -90, degrees=True).as_matrix(),
+}
+
 MARKER_SIZE = 0.02
+
+
+def get_com_pose(
+    corners: Sequence[MatLike],
+    ids: MatLike,
+    camera_matrix: npt.NDArray[np.float64],
+    distortion_coeff: npt.NDArray[np.float64],
+):
+    com_rot = []
+    com_pos = []
+
+    for corner, id in zip(corners, ids):
+        # Estimate the pose of the marker
+        rot, pos, _ = cv2.aruco.estimatePoseSingleMarkers(  # pyright: ignore[reportCallIssue]
+            corner,  # pyright: ignore[reportArgumentType]
+            MARKER_SIZE,
+            camera_matrix,
+            distortion_coeff,
+        )
+
+        if id[0] not in [0, 1, 2, 3, 4, 5]:
+            continue
+
+        # Center of mass
+        rot_mat = cv2.Rodrigues(rot)[0]
+        pos_offset = rot_mat @ trans_offset[id[0]]
+        pos_com = pos + pos_offset
+        rot_offset_mat = rot_mat @ rot_offset[id[0]]
+
+        com_rot.append(cv2.Rodrigues(rot_offset_mat)[0])
+        com_pos.append(pos_com)
+
+    return com_pos, com_rot[0]
 
 
 def detect_pose(
@@ -72,36 +120,16 @@ def detect_pose(
     if len(corners) == 0:
         return frame_vis
 
-    for corner, id in zip(corners, ids):
-        # Estimate the pose of the marker
-        rot, pos, _ = cv2.aruco.estimatePoseSingleMarkers(  # pyright: ignore[reportCallIssue]
-            corner,  # pyright: ignore[reportArgumentType]
-            MARKER_SIZE,
-            camera_matrix,
-            distortion_coeff,
-        )
+    com_pos, com_rot = get_com_pose(corners, ids, camera_matrix, distortion_coeff)
 
-        if id[0] not in [0, 1, 2, 3, 4, 5]:
-            continue
-
-        rot_mat = cv2.Rodrigues(rot)[0]
-        print(f"mat: {rot_mat.shape}, pos: {pos.flatten().shape}")
-        # print(f"Revector: {rot.flatten()}, tvec: {pos.flatten()}, id:{id}")
-
-        # Center of mass
-        pos_offset = rot_mat @ trans_offset[id[0]]
-        pos_com = pos + pos_offset
-
-        print(f"pos_com: {pos_com}, id:{id}")
-
-        frame_vis = cv2.drawFrameAxes(
-            frame,
-            camera_matrix,
-            distortion_coeff,
-            rot,
-            pos,
-            MARKER_SIZE * 0.5,
-        )
+    frame_vis = cv2.drawFrameAxes(
+        frame,
+        camera_matrix,
+        distortion_coeff,
+        com_rot,
+        com_pos,
+        MARKER_SIZE * 0.5,
+    )
 
     # Draw the detected markers
     frame_vis = cv2.aruco.drawDetectedMarkers(frame_vis, corners)
